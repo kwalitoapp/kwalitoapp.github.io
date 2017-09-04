c7e59cee8bc52b9603bfa115b9980ffce47a2604
Pierre Killy <pierre@kwali.to>
2017-09-04 12:42:55 +0200

    removed smiley

--- js/freelancer.js
+++ js/freelancer.js
@@ -83,7 +83,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                       .append("</button>");
                     $('#success > .alert-success')
-                      .append("<strong>Merci " + firstname + " ! Nous vous répondrons aussi vite que possible :)</strong>");
+                      .append("<strong>Merci " + firstname + " ! Nous vous répondrons aussi vite que possible !</strong>");
                     $('#success > .alert-success')
                       .append('</div>');
 
